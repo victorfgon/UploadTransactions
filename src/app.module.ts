9fb7a7fdb3680b1e3ae6086d2bb003873b749d3c
Victor <vitocafg@gmail.com>
2023-05-12 02:22:47 -0300

    creating modules and refactoring folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
+import { TransactionModule } from './transaction/transaction.module';
+import { BalanceModule } from './balance/balance.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TransactionModule, BalanceModule],
   controllers: [],
   providers: [],
 })
