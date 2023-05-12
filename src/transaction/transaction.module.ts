467db78e5aa798dc098b1e9c6ad6caee461228c4
Victor <vitocafg@gmail.com>
2023-05-12 04:56:55 -0300

    creating importTransactions function on service

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TransactionRepository } from './transaction.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TransactionService } from './transaction.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TransactionRepository])],
+  providers: [TransactionService],
 })
 export class TransactionModule {}
